1351b8ff6cbdd05dbc7f36bffba7b4ca68f5a26f
ragan <raganalemu3@gmail.com>
2023-04-24 16:12:26 +0300

    Update JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let books = JSON.parse(localStorage.getItem('books')) || [];
+let count = 0;
+
 // book constructor function
 function Book(title, author, id) {
   this.title = title;
@@ -24,3 +26,43 @@ function displayBooks() {
     bookList.appendChild(contain);
   });
 }
+
+// add book to collection
+function addBookToCollection(title, author, id) {
+  const newBook = new Book(title, author, id);
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books)); // save updated collection to localStorage
+  displayBooks();
+}
+
+// remove book from collection
+function removeBookFromCollection(id) {
+  books = books.filter((book) => book.id !== parseInt(id, 10));
+  localStorage.setItem('books', JSON.stringify(books)); // save updated collection to localStorage
+  displayBooks();
+}
+
+// event listener for add book form submission
+const addBookForm = document.getElementById('add-book-form');
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const titleInput = document.getElementById('title-input');
+  const authorInput = document.getElementById('author-input');
+  addBookToCollection(titleInput.value, authorInput.value, count);
+  titleInput.value = '';
+  authorInput.value = '';
+  count += 1;
+});
+
+// event listener for remove book button click
+document.addEventListener('click', (e) => {
+  const target = e.target.closest('.remove-book-button');
+  if (target) {
+    const { id } = target.parentNode;
+    removeBookFromCollection(id);
+    target.parentNode.remove();
+  }
+});
+
+// initial display of books
+displayBooks();
