9446e8b5d2064f14a191efb856c7c9489020249d
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-04-24 18:36:00 +0530

    Update script.js file

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+let books = JSON.parse(localStorage.getItem('books')) || [];
+// book constructor function
+function Book(title, author, id) {
+  this.title = title;
+  this.author = author;
+  this.id = id;
+}
 
+// display books in page
+function displayBooks() {
+  const bookList = document.querySelector('ul');
+  bookList.innerHTML = '';
+  books.forEach((book) => {
+    const contain = document.createElement('li');
+    contain.id = book.id;
+    contain.innerHTML = `
+    
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <button class='remove-book-button'>Remove</button>
+        <hr/>
+        `;
+
+    bookList.appendChild(contain);
+  });
+}
